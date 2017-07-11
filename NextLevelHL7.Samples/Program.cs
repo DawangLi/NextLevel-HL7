c6416638dcd199bb9ce5c005565c3b2196d101df
Shaun Tonstad <tonstad.shaun@mhsil.com>
2017-07-11 17:21:41 -0500

    Initial commit (continued)

---
+++ NextLevelHL7.Samples/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Threading;
+
+namespace NextLevelHL7.Samples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // sample #1: create an inbound HL7 consumer using MLLP frames over TCP/IP sockets
+            BaseHL7Interface hl7InboundSocketInterface = new HL7InboundSocketInterface("Inbound Socket Sample", 2575);
+            hl7InboundSocketInterface.MessageEvent += OnMessageEvent;
+            hl7InboundSocketInterface.StatusEvent += OnStatusEvent;
+            hl7InboundSocketInterface.ErrorEvent += OnErrorEvent;
+            hl7InboundSocketInterface.SendAcknowledgements = false;
+            hl7InboundSocketInterface.StartAsync();
+
+            // sample #2: create an outbound HL7 publisher using MLLP frames over TCP/IP sockets
+            HL7OutboundSocketInterface hl7OutboundSocketInterface = new HL7OutboundSocketInterface("Outbound Socket Sample", "127.0.0.1", 2575);
+            hl7OutboundSocketInterface.MessageEvent += OnMessageEvent;
+            hl7OutboundSocketInterface.StatusEvent += OnStatusEvent;
+            hl7OutboundSocketInterface.ErrorEvent += OnErrorEvent;
+            hl7OutboundSocketInterface.StartAsync();
+            hl7OutboundSocketInterface.EnqueueMessage(GetSampleHL7Message());
+
+            // sample #3: create an inbound HL7 consmer using messages persisted to file system
+            BaseHL7Interface hl7InboundFileSystemInterface = new HL7InboundFileSystemInterface("File System Sample", Environment.CurrentDirectory, "hl7");
+            hl7InboundFileSystemInterface.MessageEvent += OnMessageEvent;
+            hl7InboundFileSystemInterface.StatusEvent += OnStatusEvent;
+            hl7InboundFileSystemInterface.ErrorEvent += OnErrorEvent;
+            hl7InboundFileSystemInterface.StartAsync();
+
+            // sample #4: show statistics for active interfaces
+            Thread.Sleep(4000); // wait five seconds since previous HL7 logic is async
+            InterfaceStatistics statistics = hl7InboundSocketInterface.Statistics;
+            Console.WriteLine("[Statistics]: Interface Start Time: {0}", statistics.StartDateTime.ToString());
+            Console.WriteLine("[Statistics]: Interface Uptime: {0}s", statistics.UpTime.TotalSeconds);
+            Console.WriteLine("[Statistics]: Interface Last Message: {0}", statistics.LastMessageDateTime.ToString());
+            foreach (KeyValuePair<string, int> messageSuccess in statistics.Successes)
+                Console.WriteLine("[Statistics]: Interface Message {0}: {1}", messageSuccess.Key, messageSuccess.Value);
+
+            Console.ReadKey();
+
+            hl7InboundSocketInterface.Stop();
+            hl7InboundFileSystemInterface.Stop();
+            hl7OutboundSocketInterface.Stop();
+        }
+
+        /// <summary>
+        /// Demonstrates how to capture errors issued by an interface.  Errors are shared as an event
+        /// to ensure the interface continues to process incoming traffic.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The e.</param>
+        private static void OnErrorEvent(object sender, Exception e)
+        {
+            IEHRInterface ehrInterface = sender as IEHRInterface;
+            Console.WriteLine("[{0}]: {1}", ehrInterface.Name, e.ToString());
+        }
+
+
+        /// <summary>
+        /// Demonstrates how to capture an event which describes a change in interface status (i.e. Started, Stopped).
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="statusEvent">The e.</param>
+        private static void OnStatusEvent(object sender, InterfaceStatusEvent statusEvent)
+        {
+            IEHRInterface ehrInterface = sender as IEHRInterface;
+            Console.WriteLine("[{0}]: {1}", ehrInterface.Name, statusEvent.Text);
+        }
+
+        /// <summary>
+        /// Demonstrates how to parse incoming HL7 messages.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="message">The message.</param>
+        private static void OnMessageEvent(object sender, Message message)
+        {
+            IEHRInterface ehrInterface = sender as IEHRInterface;
+
+            SampleMessageProcessor sampleMessageProcessor = new SampleMessageProcessor();
+            string messageType = message.MessageType();
+
+            Console.WriteLine("[{0}]: {1} Event ", ehrInterface.Name, messageType);
+
+            switch (messageType)
+            {
+                case "ADT^A01":
+                case "ADT^A02":
+                case "ADT^A03":
+                case "ADT^A04":
+                    /// demonstrates how to read an HL7 ADT ("Admit Dischrage Transfer") message.  
+                    sampleMessageProcessor.HandleADT(message);
+                    break;
+                case "ORU^R01":
+                    sampleMessageProcessor.HandleORU(message);
+                    break;
+                case "ADT^A34":
+                    sampleMessageProcessor.HandleMerge(message);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private static string GetSampleHL7Message()
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            string resourceName = "NextLevelHL7.Samples.sample.hl7";
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            using (StreamReader streamReader = new StreamReader(stream))
+                return streamReader.ReadToEnd();
+        }
+    }
+}
---
+++ NextLevelHL7.Samples/SampleMessageProcessor.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using NextLevelHL7.Extensions;
+
+namespace NextLevelHL7.Samples
+{
+    public class SampleMessageProcessor
+    {
+        /// <summary>
+        /// Demonstrates how to read an HL7 ADT ("Admit Dischrage Transfer") message.  
+        /// </summary>
+        /// <param name="message">The HL7 message.</param>
+        public void HandleADT(Message message)
+        {
+            // message type
+            string messageType = message.MessageType();
+
+            // retrieve patient identifier
+            Segment pid = message.FindSegment("PID");
+
+            // retrieve other message types
+            Segment evn = message.FindSegment("EVN");
+            Segment pv1 = message.FindSegment("PV1");
+            Segment pv2 = message.FindSegment("PV2");
+            Segment zlo = message.FindSegment("ZLO");
+
+            // switch on message type to show admit, dischage, or transfer
+            string sampleResult = string.Empty;
+            switch (messageType)
+            {
+                case "ADT^A01":
+                    sampleResult = "Patient {0} (DOB {1}) has been admitted to {2}.  Reason: {3}.  Date: {4:g}";
+                    break;
+                case "ADT^A02":
+                    sampleResult = "Patient {0} (DOB {1}) has been transferred to {2}.  Reason: {3}.  Date: {4:g}";
+                    break;
+                case "ADT^A03":
+                    sampleResult = "Patient {0} (DOB {1}) has been discharged from {2}.  Reason: {3}.  Date: {4:g}";
+                    break;
+                case "ADT^A04":
+                    sampleResult = "Patient {0} (DOB {1}) has been admitted to {2}.  Reason: {3}.  Date: {4:g}";
+                    break;
+            }
+
+            string patientName = pid[5][0] + " " + pid[5][1] + " " + pid[5][2];
+            string dob = pid[7][0].Substring(4, 2) + "/" + pid[7][0].Substring(6, 2) + "/" + pid[7][0].Substring(0, 4);
+            string location;
+            if (zlo != null && !string.IsNullOrEmpty(zlo[2]))
+                location = zlo[2];
+            else
+                location = pv1[3][0];
+            string reason = pv2[3];
+            if (reason != null)
+                reason = reason.Trim('^');
+            DateTime? date = evn[2].ToString().ParseDate();
+
+            sampleResult = string.Format(sampleResult, patientName, dob, location, reason, date);
+        }
+
+        /// <summary>
+        /// Demonstrates how to read an HL7 ORU ("Observation Result") message
+        /// </summary>
+        /// <param name="message">The HL7 message.</param>
+        public void HandleORU(Message message)
+        {
+            // retrieve patient identifier
+            Segment pid = message.FindSegment("PID");
+            Segment segment = message.FindSegment("OBR");
+
+            List<Segment> obxSegments = message.FindSegments("OBX");
+            foreach (Segment obx in obxSegments)
+            {
+                // iterate through each result within a given obseration ("OBX")
+                string accessionID = segment[20][0];
+                string loinc = obx[3][0];
+                DateTime? resultDate = obx[14].ToString().ParseDate();
+                string resultStatus = null;
+                if (obx[11])
+                    resultStatus = obx[11];
+                string resultType = obx[3][4];
+                string resultUnits = obx[6][1];
+                bool resultIsAbnormal = obx[8] == "Y";
+                string resultReferenceRange = obx[7][0];
+                float numericResult;
+                float.TryParse(obx[5], out numericResult);
+                string valueText = obx[5].ToString();
+                if (!string.IsNullOrEmpty(valueText))
+                    valueText = (valueText.Substring(0, Math.Min(valueText.Length, 512)));
+            }
+        }
+
+        /// <summary>
+        /// Demonstrates how to read an HL7 ADT Patient Merge message
+        /// </summary>
+        /// <param name="message">The message.</param>
+        public void HandleMerge(Message message)
+        {
+            Segment pid = message.FindSegment("PID");
+            Segment mrg = message.FindSegment("MRG");
+            if (pid == null || mrg == null)
+                return;
+
+            string newMRN = pid[2][0];
+            string oldMRN = mrg[1][0];
+
+            // execute patient merge here using new and old MRN
+        }
+    }
+}
